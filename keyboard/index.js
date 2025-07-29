0c88c45cfe54ad18c529f25f58decc547c792e9f
mayershohag <shohagsreepur2021@gmail.com>
2025-07-29 17:47:00 +0600

    some change from index.js file

--- keyboard/index.js
+++ keyboard/index.js
@@ -4,10 +4,8 @@ const KeyA = document.querySelector("#KeyA")
 
 document.addEventListener("keydown", (event) => {
      let key = event.key;
-     console.log("key "+key);
      buttons.forEach((element) => {
           if (element.innerHTML === key) {
-               console.log(`innerHTML `+element.innerHTML);
                KeyA.classList.add = "key-color";
           } else {
                console.log(`incorrect`);
